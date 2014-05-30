117baf514a455ea9d9356893df1a078687a6a756
jamesstott <jamesstott1@gmail.com>
2014-05-30 13:04:05 +0200

    first commint

---
+++ data/exp_links.js
@@ -0,0 +1,11 @@
+var exp_links = {
+"type": "FeatureCollection",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+                                                                                
+"features": [
+{ "type": "Feature", "properties": { "color_qgis2leaf": '#ff0000', "radius_qgis2leaf": 2.5, "transp_qgis2leaf": 1.0, "transp_fill_qgis2leaf": 1.0, "FID": 0.0 }, "geometry": { "type": "LineString", "coordinates": [ [ 5.515426000034129, 58.917794999845945 ], [ 5.793108000013437, 58.858145999906121 ] ] } },
+{ "type": "Feature", "properties": { "color_qgis2leaf": '#ff0000', "radius_qgis2leaf": 2.5, "transp_qgis2leaf": 1.0, "transp_fill_qgis2leaf": 1.0, "FID": 1.0 }, "geometry": { "type": "LineString", "coordinates": [ [ 5.793108000013437, 58.858145999906121 ], [ 5.714884000018129, 58.991899999892063 ] ] } },
+{ "type": "Feature", "properties": { "color_qgis2leaf": '#ff0000', "radius_qgis2leaf": 2.5, "transp_qgis2leaf": 1.0, "transp_fill_qgis2leaf": 1.0, "FID": 2.0 }, "geometry": { "type": "LineString", "coordinates": [ [ 5.714884000018129, 58.991899999892063 ], [ 5.515426000034129, 58.917794999845945 ] ] } },
+{ "type": "Feature", "properties": { "color_qgis2leaf": '#ff0000', "radius_qgis2leaf": 2.5, "transp_qgis2leaf": 1.0, "transp_fill_qgis2leaf": 1.0, "FID": null }, "geometry": { "type": "LineString", "coordinates": [ [ 5.505166000034816, 58.826175000677551 ], [ 5.793108000013433, 58.858146000740462 ] ] } }
+]
+}
